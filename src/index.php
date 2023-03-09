7b2011b523e0b08ea86ba3cd96f54e1507d7dc14
Imtiaz Pabel <pabel@Imtiaz-2.local>
2023-03-09 16:39:13 +0600

    first commit

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Example;
+
+$object = new Example;
+echo $object->hello();
---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_it_just_example()
+    {
+        $this->assertSame(true, true);
+    }
+}
