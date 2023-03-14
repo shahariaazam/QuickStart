906812acebc49b35c2326644042bb32f55eafdc1
xxxx xxxxxx <xxxx@xxx.xxx>
2023-03-14 09:21:12 +0100

    Few improvements

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,11 +1,51 @@
 <?php
 
+declare(strict_types=1);
+
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-    public function test_it_just_example()
+    private App\Example $example;
+
+    public function setUp(): void
     {
-        $this->assertSame(true, true);
+        $this->example = new App\Example();
+    }
+
+    public function testHello()
+    {
+        $this->assertSame("hi", $this->example->hello());
+    }
+
+    /**
+     * @dataProvider dataProviderForAddition
+     *
+     * @return void
+     */
+    public function testAddition(int $numberOne, int $numberTwo, int $expectedResult)
+    {
+        $this->assertSame($expectedResult, $this->example->addition($numberOne, $numberTwo));
+    }
+
+    public function dataProviderForAddition() :array
+    {
+        return [
+            'two integer' => [
+                1,
+                2,
+                3
+            ],
+            'one positive number and one negative number' => [
+                1,
+                -2,
+                -1
+            ],
+            'one positive large number than second number' => [
+                -1,
+                2,
+                1
+            ]
+        ];
     }
 }
